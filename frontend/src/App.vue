86bbfab0778f8a9becfed3c3ffbcbf61057699ae
maet98 <maletavaeras@gmail.com>
2020-12-22 13:06:13 -0400

    skeleton of front end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="App">
+        <div id="nav">
+            <router-link to="/">Dashboard</router-link> |
+            <router-link to="/usuarios">Usuario</router-link> |
+            <router-link to="/compras">Ver Compras</router-link>
+        </div>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+
+export default {
+};
+</script>
 
 <style>
 #app {
@@ -26,7 +33,14 @@
   color: #2c3e50;
 }
 
+.logout {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+}
+
 #nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="addNew()">Randomize</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import LineChart from './LineChart.js'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null,
+        data: [],
+        labels: []
+      }
+    },
+    mounted () {
+      this.addNew()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+            labels: this.labels,
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#4287f5',
+              data: this.data
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      },
+        addNew() {
+            this.labels.push(this.labels.length + 1)
+            this.data.push(this.getRandomInt())
+            this.datacollection = {
+                labels: this.labels,
+              datasets: [
+                {
+                  label: 'Data One',
+                  backgroundColor: '#4287f5',
+                  data: this.data
+                }
+              ]
+            }
+        }
+    }
   }
-}
 </script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
